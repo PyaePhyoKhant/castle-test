b2f6e5c78a897319ff47a70e6b80330e1ccb2aef
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2019-04-10 21:46:42 +0630

    update home page

--- items/views.py
+++ items/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from items.forms import ItemForm
 from items.models import Item
 
 
-# Create your views here.
 def add(request):
     if request.method == 'GET':
         context = {'form': ItemForm}
@@ -13,4 +12,9 @@ def add(request):
         Item.objects.create(name=request.POST['name'],
                             photo=request.POST['photo'],
                             created_by=request.user)
-        return render(request, 'home.html')
+        return redirect('home')
+
+
+def home(request):
+    context = {'items': Item.objects.filter(created_by=request.user)}
+    return render(request, 'home.html', context)
