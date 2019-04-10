1dc6f234d6cd112e078f22ce507450e1aa7bc8b5
Pyae Phyo Khant <lanmadawsmith@gmail.com>
2019-04-10 22:00:53 +0630

    login redirect

--- items/views.py
+++ items/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from items.forms import ItemForm
 from items.models import Item
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url='/login/')
 def add(request):
     if request.method == 'GET':
         context = {'form': ItemForm}
@@ -15,6 +19,7 @@ def add(request):
         return redirect('home')
 
 
+@login_required(login_url='/login/')
 def home(request):
     context = {'items': Item.objects.filter(created_by=request.user)}
     return render(request, 'home.html', context)
